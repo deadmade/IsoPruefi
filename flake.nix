01a3268161d8e6c75376f6af90689d5b09644ec5
deadmade <manuel.schuelein@proton.me>
2025-07-07 16:02:15 +0200

    feat: initial Commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "PGA-SE-KSTH dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs, ... } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+	pkgs.dotnet-sdk_9
+	pkgs.nodejs
+	pkgs.python313
+	pkgs.python313Packages.pip
+        pkgs.docker
+        pkgs.lazydocker
+      ];
+            shellHook = ''
+              npm install
+            
+              python -m venv .venv
+              source .venv/bin/activate
+              pip install -r docs/requirements.txt
+
+              cd frontend
+              npm install
+              cd ..
+            '';
+    };
+  };
+}

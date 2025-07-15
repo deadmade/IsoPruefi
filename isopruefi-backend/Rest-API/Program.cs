129b3a0bb6298ba02ad1b2eb8494ed1f70f9b5a0
deadmade <130925709+deadmade@users.noreply.github.com>
2025-07-15 13:47:34 +0200

    feat: added swagger ui to rest-api + generation of api definition

--- isopruefi-backend/Rest-API/Program.cs
+++ isopruefi-backend/Rest-API/Program.cs
@@ -12,12 +12,18 @@ public class Program
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(); // UI is available at /swagger
         }
 
         app.UseHttpsRedirection();

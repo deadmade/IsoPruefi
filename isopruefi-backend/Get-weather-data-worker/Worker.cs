c24d69d989d86b01899696b774527e8165fb1b9f
maratin23 <157828439+maratin23@users.noreply.github.com>
2025-07-22 13:30:30 +0200

    feat: initial Get-weather-data-worker project structure

---
+++ isopruefi-backend/Get-weather-data-worker/Worker.cs
@@ -0,0 +1,18 @@
+namespace Get_weather_data_worker;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public Worker(ILogger<Worker> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        HttpClient httpClient = _httpClientFactory.CreateClient();
+    }
+}

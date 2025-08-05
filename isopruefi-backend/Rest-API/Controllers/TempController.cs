36091dcb3c807272a93311b135a937af42f1ef74
maratin23 <157828439+maratin23@users.noreply.github.com>
2025-08-05 17:13:37 +0200

    feat: added controller for TempService

---
+++ isopruefi-backend/Rest-API/Controllers/TempController.cs
@@ -0,0 +1,69 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Rest_API.Services.Temp;
+
+namespace Rest_API.Controllers;
+
+/// <summary>
+/// Controller for managing temperature data actions. Provides endpoints for retrieving postalcodes and getting locations.
+/// </summary>
+[ApiVersion(1)]
+[ApiController]
+[Route("api/v{v:apiVersion}/[controller]/[action]")]
+[Produces("application/json")]
+[Consumes("application/json")]
+public class TempController : ControllerBase
+{
+    private readonly ITempService _tempService;
+    private readonly ILogger<TempController> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TempController"/> class.
+    /// </summary>
+    /// <param name="tempService">The temp service to handle temperature operations.</param>
+    /// <param name="logger">The logger instance for logging actions and errors.</param>
+    public TempController(ITempService tempService, ILogger<TempController> logger)
+    {
+        _tempService = tempService;
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Retrieves all saved locations.
+    /// </summary>
+    /// <returns>A list of all postalcodes; otherwise, NotFound.</returns>
+    [HttpGet]
+    public async Task<ActionResult> GetAllPostalcodes()
+    {
+        try
+        {
+            var postalcodes = await _tempService.ShowAvailablePostalcodes();
+            return Ok(postalcodes);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error fetching all postalcodes");
+            return StatusCode(StatusCodes.Status500InternalServerError, new ProblemDetails { Detail = e.Message });
+        }
+    }
+
+    /// <summary>
+    /// Checks for existence of location and if necessary inserts new location.
+    /// </summary>
+    /// <param name="postalcode">Defines the location.</param>
+    /// <returns>Ok if successful; otherwise, an error response.</returns>
+    [HttpPost]
+    public async Task<ActionResult> InsertLocation([FromBody] int postalcode)
+    {
+        try
+        {
+            await _tempService.GetCoordinates(postalcode);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error while inserting a location");
+            return StatusCode(StatusCodes.Status500InternalServerError, new ProblemDetails { Detail = e.Message });
+        }
+    }
+}

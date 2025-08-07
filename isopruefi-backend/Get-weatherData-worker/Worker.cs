a72c9f8ad576086cb4f48104b0a70722da7eae69
maratin23 <157828439+maratin23@users.noreply.github.com>
2025-08-07 09:18:36 +0200

    refactor: get weather data worker for readability

--- isopruefi-backend/Get-weatherData-worker/Worker.cs
+++ isopruefi-backend/Get-weatherData-worker/Worker.cs
@@ -41,113 +41,65 @@ public class Worker : BackgroundService
             using var scope = _serviceProvider.CreateScope();
             var settingsRepo = scope.ServiceProvider.GetRequiredService<ISettingsRepo>();
             var influxRepo = scope.ServiceProvider.GetRequiredService<IInfluxRepo>();
-            var weatherData = new WeatherData();
             
             // Getting the location information for the next unlocked entry.
             var result = await GetAvailableCoordinateMapping(settingsRepo);
-            if (result != null)
-            { 
-                var lat = result.Latitude; 
-                var lon = result.Longitude;
+            
+            if (result == null)
+            {
+                _logger.LogInformation("All locations have up to date weather data.");
+                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                continue;
+            }
+
+            var lat = result.Latitude; 
+            var lon = result.Longitude;
                 
-                // Sending GET-Request to Meteo.
-                var response = await callMeteoApi(lat, lon);
+            // Sending GET-Request to Meteo.
+            var response = await CallMeteoApi(lat, lon);
                 
-                if (response.IsSuccessStatusCode)
+            if (response != null)
+            {
+                // Saving the temperature in the database.
+                try
                 {
-                    // Getting temperature data from the Meteo response.
-                    using var json = JsonDocument.Parse(await response.Content.ReadAsStreamAsync());
-                    var root = json.RootElement;
-                    if (root.TryGetProperty("current", out var current))
+                    await influxRepo.WriteOutsideWeatherData(_location, "Meteo", response.Temperature,
+                        response.Timestamp);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Outside Weather data could not be saved in the database.");
+                }
+            }
+            else
+            {
+                // Sending GET-Request to Bright Sky.
+                var alternativeResponse = await CallBrightSkyApi(lat, lon);
+
+                if (alternativeResponse != null)
+                {
+                    // Saving the temperature in the database.
+                    try
                     {
-                        // Getting the time and temperature data from the JSON file.
-                        if (current.TryGetProperty("time", out var time) &&
-                            current.TryGetProperty("temperature_2m", out var temperature))
-                        {
-                            weatherData.Timestamp = time.GetDateTime();
-                            weatherData.Temperature = temperature.GetDouble();
-
-                            // Saving the temperature in the database.
-                            try
-                            {
-                                await influxRepo.WriteOutsideWeatherData(_location, "Meteo", weatherData.Temperature,
-                                    weatherData.Timestamp);
-                            }
-                            catch (Exception e)
-                            {
-                                _logger.LogError(e, "Outside Weather data could not be saved in the database.");
-                            }
-
-                            _logger.LogInformation("Weather data from Meteo retrieved successfully.");
-                        }
-                        else
-                        {
-                            _logger.LogError("Data from Meteo incomplete.");
-                        }
+                        await influxRepo.WriteOutsideWeatherData(_location, "Bright Sky",
+                            alternativeResponse.Temperature, alternativeResponse.Timestamp);
                     }
-                    else
+                    catch (Exception e)
                     {
-                        _logger.LogError("Data from Meteo incomplete.");
+                        _logger.LogError(e, "Outside Weather data could not be saved in the database.");
                     }
                 }
                 else
                 {
-                    // Sending GET-Request to Bright Sky.
-                    var alternativeResponse = await callBrightSkyApi(lat, lon);
-
-                    if (alternativeResponse.IsSuccessStatusCode)
-                    {
-                        // Getting temperature data from the Bright Sky response.
-                        using var alternativeJson =
-                            JsonDocument.Parse(await alternativeResponse.Content.ReadAsStreamAsync());
-                        var root = alternativeJson.RootElement;
-                        if (root.TryGetProperty("weather", out var weather))
-                        {
-                            // Getting the time and temperature data from the JSON file.
-                            if (weather.TryGetProperty("timestamp", out var time) &&
-                                weather.TryGetProperty("temperature", out var temperature))
-                            {
-                                weatherData.Timestamp = time.GetDateTime();
-                                weatherData.Temperature = temperature.GetDouble();
-
-                                // Saving the temperature in the database.
-                                try
-                                {
-                                    await influxRepo.WriteOutsideWeatherData(_location, "Bright Sky", 
-                                        weatherData.Temperature, weatherData.Timestamp);
-                                }
-                                catch (Exception e)
-                                {
-                                    _logger.LogError(e, "Outside Weather data could not be saved in the database.");
-                                }
-
-                                _logger.LogInformation("Weather data from Bright Sky retrieved successfully.");
-                            }
-                            else
-                            {
-                                _logger.LogError("Data from Bright Sky incomplete.");
-                            }
-                        }
-                        else
-                        {
-                            _logger.LogError("Data from Bright Sky incomplete.");
-                        }
-                    }
-                    else
-                    {
-                        _logger.LogError("Failed to retrieve data from both sources.");
-                    }
+                    _logger.LogError("Failed to retrieve data from both sources.");
                 }
-            }   
-            else
-            {
-                _logger.LogInformation("All locations have up to date weather data.");
             }
+
             await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
         }
     }
 
-    private async Task<CoordinateMapping?> GetAvailableCoordinateMapping(ISettingsRepo? settingsRepo)
+    private async Task<CoordinateMapping?> GetAvailableCoordinateMapping(ISettingsRepo settingsRepo)
     {
         try
         {
@@ -162,20 +114,50 @@ public class Worker : BackgroundService
         return null;
     }
 
-    private async Task<HttpResponseMessage> callMeteoApi(double lat, double lon)
+    private async Task<WeatherData?> CallMeteoApi(double lat, double lon)
     {
         var httpClient = _httpClientFactory.CreateClient();
-        
+
         var weatherDataApi = _weatherDataApi
             .Replace("{lat}", lat.ToString())
             .Replace("{lon}", lon.ToString());
-        
+
         var response = await httpClient.GetAsync(weatherDataApi);
 
-        return response;
+        if (response.IsSuccessStatusCode)
+        {
+            // Getting temperature data from the Meteo response.
+            using var json = JsonDocument.Parse(await response.Content.ReadAsStreamAsync());
+            var root = json.RootElement;
+            if (root.TryGetProperty("current", out var current))
+            {
+                // Getting the time and temperature data from the JSON file.
+                if (current.TryGetProperty("time", out var time) &&
+                    current.TryGetProperty("temperature_2m", out var temperature))
+                {
+                    var weatherData = new WeatherData();
+                    weatherData.Timestamp = time.GetDateTime();
+                    weatherData.Temperature = temperature.GetDouble();
+
+                    _logger.LogInformation("Weather data from Meteo retrieved successfully.");
+
+                    return weatherData;
+                }
+                else
+                {
+                    _logger.LogWarning("Weather data from Meteo incomplete");
+                }
+            }
+        }
+        else
+        {
+            _logger.LogWarning("HTTP Request to Meteo failed with status code: " + response.StatusCode);
+        }
+        
+        return null;
     }
-    
-    private async Task<HttpResponseMessage> callBrightSkyApi(double lat, double lon)
+
+    private async Task<WeatherData?> CallBrightSkyApi(double lat, double lon)
     {
         var httpClient = _httpClientFactory.CreateClient();
         
@@ -185,6 +167,36 @@ public class Worker : BackgroundService
         
         var response = await httpClient.GetAsync(weatherDataApi);
 
-        return response;
+        if (response.IsSuccessStatusCode)
+        {
+            // Getting temperature data from the Bright Sky response.
+            using var alternativeJson =
+                JsonDocument.Parse(await response.Content.ReadAsStreamAsync());
+            var root = alternativeJson.RootElement;
+            if (root.TryGetProperty("weather", out var weather))
+            {
+                // Getting the time and temperature data from the JSON file.
+                if (weather.TryGetProperty("timestamp", out var time) &&
+                    weather.TryGetProperty("temperature", out var temperature))
+                {
+                    var weatherData = new WeatherData();
+                    weatherData.Timestamp = time.GetDateTime();
+                    weatherData.Temperature = temperature.GetDouble();
+
+                    _logger.LogInformation("Weather data from Bright Sky retrieved successfully.");
+                    return weatherData;
+                }
+                else
+                {
+                    _logger.LogWarning("Data from Bright Sky incomplete.");
+                }
+            }
+        }
+        else
+        {
+            _logger.LogWarning("HTTP Request to Bright Sky failed with status code: " + response.StatusCode);
+        }
+        
+        return null;
     }
 }

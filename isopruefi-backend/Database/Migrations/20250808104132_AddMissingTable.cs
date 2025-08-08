cd1ac8acf3a65df0d2d8f7cc330b048bc71e41dc
maratin23 <157828439+maratin23@users.noreply.github.com>
2025-08-08 12:56:06 +0200

    fix: migrations and add seeding for location "Heidenheim"

---
+++ isopruefi-backend/Database/Migrations/20250808104132_AddMissingTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CoordinateMappings",
+                columns: new[] { "PostalCode", "LastUsed", "Latitude", "Location", "LockedUntil", "Longitude" },
+                values: new object[] { 89518, null, 48.685200000000002, "Heidenheim an der Brenz", null, 10.1287 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CoordinateMappings",
+                keyColumn: "PostalCode",
+                keyValue: 89518);
+        }
+    }
+}

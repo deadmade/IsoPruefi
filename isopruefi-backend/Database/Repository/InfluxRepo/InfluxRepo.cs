e6a62340aebadd107af8aac9a156aff8167b632f
maratin23 <157828439+maratin23@users.noreply.github.com>
2025-08-19 18:04:37 +0200

    fix: mqtt tempSensorMeta and mqtt sender

--- isopruefi-backend/Database/Repository/InfluxRepo/InfluxRepo.cs
+++ isopruefi-backend/Database/Repository/InfluxRepo/InfluxRepo.cs
@@ -1,3 +1,4 @@
+using System.Threading.RateLimiting;
 using InfluxDB3.Client;
 using InfluxDB3.Client.Write;
 using Microsoft.Extensions.Configuration;
@@ -74,11 +75,31 @@ public class InfluxRepo : IInfluxRepo
     /// <inheritdoc />
     public IAsyncEnumerable<PointDataValues> GetOutsideWeatherData(DateTime start, DateTime end, string place)
     {
+        var timespan = end - start;
+        string query;
+
+        if (timespan.Hours < 24)
+        {
+            query =
+                $"SELECT place, time, value FROM outside_temperature where place='{place}' AND time BETWEEN TIMESTAMP '{start:yyyy-MM-dd HH:mm:ss}' AND TIMESTAMP '{end:yyyy-MM-dd HH:mm:ss}'";
+        }
+        else if (timespan.Days < 30)
+        {
+            query =
+                $"SELECT MEAN(time), MEAN(value) FROM outside_temperature WHERE place='{place}' AND time BETWEEN TIMESTAMP '{start:yyyy-MM-dd HH:mm:ss}' AND TIMESTAMP '{end:yyyy-MM-dd HH:mm:ss}' GROUP BY time(1h)";
+
+            return _client.QueryPoints(query);
+        }
+        else
+        {
+            query =
+                $"SELECT MEAN(time), MEAN(value) FROM outside_temperature WHERE place='{place}' AND time BETWEEN TIMESTAMP '{start:yyyy-MM-dd HH:mm:ss}' AND TIMESTAMP '{end:yyyy-MM-dd HH:mm:ss}' GROUP BY time(1d)";
+
+            return _client.QueryPoints(query);
+        }
+
         try
         {
-            var query =
-                $"SELECT place, time, value FROM outside_temperature where place='{place}' AND time BETWEEN TIMESTAMP '{start:yyyy-MM-dd HH:mm:ss}' AND TIMESTAMP '{end:yyyy-MM-dd HH:mm:ss}'";
-
             return _client.QueryPoints(query);
         }
         catch (Exception e)
--- isopruefi-backend/MQTT-Receiver-Worker/MQTT/Connection.cs
+++ isopruefi-backend/MQTT-Receiver-Worker/MQTT/Connection.cs
@@ -244,11 +244,16 @@ public class Connection : IConnection
             return Task.FromResult(Task.CompletedTask);
         }
 
-        if (tempSensorReading.Meta != null && tempSensorReading.Meta.Sequence.Length != 0 
-                                           && tempSensorReading.Meta.Sequence.Length ==
-                                           tempSensorReading.Meta.Timestamp.Length
-                                           && tempSensorReading.Meta.Sequence.Length ==
-                                           tempSensorReading.Meta.Value.Length)
+        if (tempSensorReading.Meta is { Timestamp: null, Sequence: null, Value: null })
+        {
+            _logger.LogWarning("Received sensor reading with unexpected format in meta from {SensorName}. Skipping processing",
+                sensorName);
+            return Task.FromResult(Task.CompletedTask);
+        }
+
+        if (tempSensorReading.Meta.Sequence.Length != 0
+            && tempSensorReading.Meta.Sequence.Length == tempSensorReading.Meta.Timestamp.Length
+            && tempSensorReading.Meta.Sequence.Length == tempSensorReading.Meta.Value.Length)
         {
             var tempDataMeta = tempSensorReading.Meta;
             await Parallel.ForEachAsync(Enumerable.Range(0, tempDataMeta.Sequence.Length - 1), 

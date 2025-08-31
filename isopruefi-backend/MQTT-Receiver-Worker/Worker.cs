86177fc39a7e61e3f380bad49957407ce243a1a4
deadmade <130925709+deadmade@users.noreply.github.com>
2025-08-31 12:27:09 +0200

    feat: added resubscription to topics

--- isopruefi-backend/MQTT-Receiver-Worker/Worker.cs
+++ isopruefi-backend/MQTT-Receiver-Worker/Worker.cs
@@ -100,15 +100,21 @@ public class Worker : BackgroundService
     /// <returns>A task representing the asynchronous operation.</returns>
     private async Task MonitorConnection(CancellationToken stoppingToken)
     {
+        var lastSubscription = DateTime.UtcNow;
         while (!stoppingToken.IsCancellationRequested && _connection.IsConnected)
         {
-            await Task.Delay(_healthCheckInterval, stoppingToken);
-
+            await Task.Delay(_healthCheckInterval, stoppingToken); 
+            
             if (!_connection.IsConnected)
             {
                 _logger.LogWarning("MQTT connection lost. Will attempt to reconnect...");
                 break;
             }
+
+            if (lastSubscription.AddHours(1) >= DateTime.UtcNow) continue;
+            
+            await _receiver.SubscribeToTopics();
+            _logger.LogInformation("Successfully subscribed to MQTT topics");
         }
     }
 }

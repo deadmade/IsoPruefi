04b60df36399b70809ebde8740f2c6e53d3db213
maratin23 <157828439+maratin23@users.noreply.github.com>
2025-09-02 11:23:12 +0200

    feat: update grafana dashboard

--- isopruefi-backend/Rest-API/Helper/HealthCheck.cs
+++ isopruefi-backend/Rest-API/Helper/HealthCheck.cs
@@ -10,6 +10,6 @@ public static class HealthCheck
         builder.Services.AddHealthChecks()
             .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty, "select 1",
                 name: "PostgreSQL", failureStatus: HealthStatus.Unhealthy, tags: new[] { "Database" })
-            .AddCheck<InfluxHealthCheck>("InfluxDB", HealthStatus.Unhealthy, new[] { "Database" })
+            .AddCheck<InfluxHealthCheck>("InfluxDB", HealthStatus.Unhealthy, new[] { "Database" });
     }
 }
